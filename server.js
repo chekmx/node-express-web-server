fd4bac7991a3e7f0aac64078b7d2699b9006aa9a
chekmx <stephen.john.richards@gmail.com>
2017-10-29 11:37:36 +0100

    Initial commit of express webserver application.

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs'); 
+var app = express(); 
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        console.log('Unable to append to log');
+    })
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintainance.hbs')
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+// app.get('/', (req, res) =>{
+//     // res.send('<h1>Hello Express!</h1>');
+//     res.send({
+//         name: 'Stephen',
+//         likes: ['Snowboarding','Coding']
+//     });
+// });
+
+app.get('/', (req, res) => {
+    res.render('home.hbs',{
+        pageTitle: 'Welcome Page',
+        welcomeMessage: 'Oi oi savaloy'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Something went wrong!'
+    })
+});
+
+app.listen(3000, ()=>{
+    console.log('Server started on port 3000');
+});
